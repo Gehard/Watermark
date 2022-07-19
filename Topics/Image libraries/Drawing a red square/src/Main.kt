9c1a9b91cd4a689c1ec5493709d51891df9130ae
gehard <gehardnjoro@gmail.com>
2022-07-19 16:17:59 +0300

    Drawing three strings

--- Topics/Image libraries/Drawing a red square/src/Main.kt
+++ Topics/Image libraries/Drawing a red square/src/Main.kt
@@ -1,6 +1,13 @@
 import java.awt.Color
-import java.awt.image.BufferedImage  
+import java.awt.image.BufferedImage
+const val H1 = 100
+const val H3 = 300
+const val H5 = 500
 
 fun drawSquare(): BufferedImage {
-  // Add your code here
+    val image = BufferedImage(H5, H5, BufferedImage.TYPE_INT_RGB)
+    val graph = image.createGraphics()
+    graph.color = Color.RED
+    graph.drawRect(H1, H1, H3, H3)
+    return image
 }
